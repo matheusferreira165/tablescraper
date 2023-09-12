0b62e16f0a4b2d22a1b6b70f48224098d3ed762c
Matheus Ferreira <matheus.ferreira165@outlook.com>
2023-09-11 22:21:50 -0300

    feat: instanciando generatecsv test

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,6 @@ import "github.com/matheusferreira165/tablescraper/services"
 
 func main() {
 
-	link := "https://www.w3schools.com/html/html_tables.asp"
-
-	services.ExtractTable(link)
+	services.GenerateCsv()
 
 }
